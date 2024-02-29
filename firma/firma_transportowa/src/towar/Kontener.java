d329790c6f2b19236c24c64af159bcc189a917f3
KaFkAaAaAaAa <153621289+KaFkAaAaAaAa@users.noreply.github.com>
2024-02-29 13:28:24 +0100

    Add files via upload

---
+++ firma/firma_transportowa/src/towar/Kontener.java
@@ -0,0 +1,16 @@
+package towar;
+
+public class Kontener {
+    private Towar towar;
+    public boolean zaladuj(Towar towar){
+        if (this.towar == null){
+            this.towar = towar;
+            return true;
+        }
+        return false;
+    }
+
+    public void rozladuj(){
+        towar = null;
+    }
+}
